eab44a07333a3891adfecb84e2076c0cdd031dd2
LittleBugEmpire <littlebugempire@gmail.com>
2023-11-08 15:04:18 +0530

    Initial commit

---
+++ src/main/java/com/limbo/eask/EaskApplication.java
@@ -0,0 +1,13 @@
+package com.limbo.eask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/limbo/eask/EaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limbo.eask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
